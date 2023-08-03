b2a879c75b688416c431527f3293d2bed78c91e7
vaidehi <vaidehi117v@gmail.com>
2023-08-03 18:31:10 -0400

    controller/model

--- models/post.js
+++ models/post.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 
+const likesSchema = mongoose.Schema({
+    username: String,
+    //One user has many likes, referencing because we have user model, so we can get the users information when we need it
+    userId: {
+        type: mongoose.Schema.Types.ObjectId
+    }
+})
+
 const postSchema = new mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId, 
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
     photoUrl: String,
     caption: String,
+    // One Post has many likes, we are using embedding, because the likes will always be tied to the post, so no reason
+    // to make a likes model
+    likes: [likesSchema]
 })
 
 module.exports = mongoose.model('Post', postSchema);
